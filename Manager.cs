0fd3e2650803b220b082372e9368f241792ad967
ImTiara <im.tiara@hotmail.com>
2021-03-27 12:24:59 +0100

    Create Manager.cs

---
+++ Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace ImmersiveTouch
+{
+    public static class Manager
+    {
+        public static VRCPlayer GetLocalVRCPlayer() => VRCPlayer.field_Internal_Static_VRCPlayer_0;
+
+        public static VRCPlayerApi GetLocalVRCPlayerApi() => GetLocalVRCPlayer().prop_VRCPlayerApi_0;
+
+        public static GameObject GetLocalAvatarObject() => GetLocalVRCPlayer().prop_VRCAvatarManager_0.prop_GameObject_0;
+    }
+}
