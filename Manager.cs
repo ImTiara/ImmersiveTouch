d6b9f640770e540b0b811f081363804a5e1234b9
ImTiara <im.tiara@hotmail.com>
2021-03-28 23:20:57 +0200

    Update Manager.cs

--- Manager.cs
+++ Manager.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using MelonLoader;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using UnhollowerBaseLib;
 using UnityEngine;
 using VRC.SDKBase;
 
@@ -15,5 +15,30 @@ namespace ImmersiveTouch
         public static VRCPlayerApi GetLocalVRCPlayerApi() => GetLocalVRCPlayer().prop_VRCPlayerApi_0;
 
         public static GameObject GetLocalAvatarObject() => GetLocalVRCPlayer().prop_VRCAvatarManager_0.prop_GameObject_0;
+
+        public static Il2CppArrayBase<DynamicBoneCollider> GetDynamicBoneColliders(this Animator animator, HumanBodyBones bone) => animator.GetBoneTransform(bone).GetComponentsInChildren<DynamicBoneCollider>();
+
+        public static void UIExpansionKit_RegisterSettingAsStringEnum(string categoryName, string settingName, IList<(string SettingsValue, string DisplayName)> possibleValues)
+        {
+            if (_UIExpansionKit == null) return;
+
+            _RegisterSettingAsStringEnum.Invoke(null, new object[] { categoryName, settingName, possibleValues });
+        }
+
+        public static void RegisterUIExpansionKit()
+        {
+            _UIExpansionKit = MelonHandler.Mods.FirstOrDefault(x => x.Info.Name == "UI Expansion Kit")?.Assembly;
+
+            if (_UIExpansionKit == null)
+            {
+                MelonLogger.Warning("For the best experience, it is highly recommended to use UIExpansionKit.");
+                return;
+            }
+
+            _RegisterSettingAsStringEnum = _UIExpansionKit.GetType("UIExpansionKit.API.ExpansionKitApi").GetMethod("RegisterSettingAsStringEnum");
+        }
+
+        private static Assembly _UIExpansionKit { get; set; }
+        private static MethodInfo _RegisterSettingAsStringEnum { get; set; }
     }
 }
